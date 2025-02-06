a61f0bea137f966fd02bff1bde7bf68a1365d24c
Alithebest-coder <aurumprint@proton.me>
2025-02-06 17:53:40 +1000

    Add files via upload

--- class_paroot.py
+++ class_paroot.py
@@ -3,7 +3,7 @@ class dog:
     def __init__(self,name,age):
         self.a=name
         self.g=age
-obj=dog("bob",1)
-obj2=dog("bob jr",0)
+obj=dog("dog",47)
+obj2=dog("bob jr",10)
 print (obj.species,obj.a,obj.g)
 print (obj2.species,obj2.a,obj2.g)
